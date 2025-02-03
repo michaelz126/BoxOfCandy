949a14b21a0192c41d138524d8784af2f1eef36c
michaelz126 <1zhang.michael@gmail.com>
2025-02-03 14:31:17 +0000

    first commit

---
+++ Candy.java
@@ -0,0 +1,14 @@
+public class Candy {
+    private String flavor;
+
+    public Candy(String f)
+    {
+        flavor = f;
+    }
+
+    public String getFlavor() {
+        return flavor; }
+
+    // There may be instance variables, constructors, and methods that are not
+    // shown.
+}
---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static  void main(String[] args) {
+        Candy[][] box = new Candy[4][3];
+        box[][] = new Candy("");
+        box[][] = new Candy("");
+        box[][] = new Candy("");
+        box[][] = new Candy("");
+        box[][] = new Candy("");
+
+    }
+}
